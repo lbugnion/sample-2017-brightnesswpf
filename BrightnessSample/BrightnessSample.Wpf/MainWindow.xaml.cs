62da3b57f9db9328b11af6436475c3a70918c6b1
LBugnion <LBugnion@valorem.com>
2017-04-15 13:46:50 +0200

    First commit

---
+++ BrightnessSample/BrightnessSample.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Management;
+using System.Windows;
+
+namespace BrightnessSample.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int _currentValue;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BrightnessButtonClick(object sender, RoutedEventArgs e)
+        {
+            _currentValue += 10; // percents
+
+            if (_currentValue > 100)
+            {
+                _currentValue = 0; // cycle
+            }
+
+            // Get the class by executing the query
+            var searcher = new ManagementObjectSearcher("root\\WMI", "SELECT * FROM WmiMonitorBrightness");
+            var results = searcher.Get();
+            var resultEnum = results.GetEnumerator();
+            resultEnum.MoveNext();
+
+            var result = resultEnum.Current;
+            var instanceName = (string)result["InstanceName"];
+
+            // Create the instance
+            var classInstance = new ManagementObject(
+                "root\\WMI", 
+                "WmiMonitorBrightnessMethods.InstanceName='" + instanceName + "'", 
+                null);
+
+            // Get the parameters for the method
+            var inParams = classInstance.GetMethodParameters("WmiSetBrightness");
+            inParams["Brightness"] = _currentValue;
+            inParams["Timeout"] = 0;
+
+            classInstance.InvokeMethod("WmiSetBrightness", inParams, null);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,52 @@
+# sample-evolve-2016
+My code samples for the Xamarin Evolve 2016 conference
+
+# Presentation details, Slides, Video
+
+You can see the video recording and the slides here:
+
+http://galasoft.ch/presentations/presentationdetails.cshtml?name=2016006
+
+# Content
+
+## XamBindingSample
+
+Contains binding samples for iOS, Android and Windows
+
+- Simple binding VM -> UI element
+- Simple binding UI element -> UI element with default event handling
+- Simple binding UI element -> UI element with custom event handling
+- Twoway binding with default conversion
+- Twoway binding with custom conversion
+- Using WhenSourceChanges
+- Saving bindings
+- Using TargetNullValue and FallbackValue
+- Avoiding linker issues
+
+Contains Commanding sample for iOS, Android, Windows
+
+- Simple command with default event handling
+- Command with custom event handling
+- Command with static parameter
+- Command with dynamic parameter (binding)
+- Avoiding linker issues
+
+Contains List sample for Android and Windows
+
+- RecyclerView
+- Binding to ObservableCollection
+- Use the SelectedItem
+- Add and remove elements from collection
+- Binding between list cell UI element and data item
+- Changing UI element appearance on click
+
+## ObservableListApple 
+
+Contains List sample for iOS
+
+- RecyclerView
+- Binding to ObservableCollection
+- Use the SelectedItem
+- Add and remove elements from collection
+- Binding between list cell UI element and data item
+- Changing UI element appearance on click
